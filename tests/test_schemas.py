f7f9dc10b795a38e3b5edc25b7cbca4ded619fab
carlospinheirof <carlos.pinheiro.f@gmail.com>
2022-11-11 01:45:15 -0300

    Add tests to schemas

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -25,3 +25,40 @@ def test_valid_costumer(custom_costumer):
         assert False
     else:
         assert True
+
+
+def test_missing_values_costumer():
+    try:
+        test_costumer = {
+            'corporate_name': 'custom',
+            'address': 'addr',
+            'declared_billing': '1235'
+        }
+        _ = CustomerBaseSchema(**test_costumer)
+    except ValueError:
+        assert True
+    else:
+        assert False
+
+
+def test_invalid_account_costumer(custom_costumer):
+    try:
+        test_costumer = custom_costumer
+        test_costumer['bank_data'][0]['account'] = 'abc'
+
+        _ = CustomerBaseSchema(**test_costumer)
+    except ValueError:
+        assert True
+    else:
+        assert False
+
+def test_invalid_bank_branch_costumer(custom_costumer):
+    try:
+        test_costumer = custom_costumer
+        test_costumer['bank_data'][0]['bank_branch'] = 'abc'
+
+        _ = CustomerBaseSchema(**test_costumer)
+    except ValueError:
+        assert True
+    else:
+        assert False
