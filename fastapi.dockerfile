c6cf925f76ee5e9e791884aa5d8f24fa01f1c650
carlospinheirof <carlos.pinheiro.f@gmail.com>
2022-11-11 01:53:55 -0300

    Feature add schemas (#1)
    * Add schemas and modify dockerfile to better development
    
    * Add tests to schemas
    
    * Fix lint

--- fastapi.dockerfile
+++ fastapi.dockerfile
@@ -1,9 +1,8 @@
 FROM python:3.10.8
 
 LABEL author="José Carlos Pinheiro Filho"
-ENV PORT=8080
-COPY . /app
+ENV PORT=8000
 WORKDIR /app
+COPY requirements.txt /app
 RUN pip install -r requirements.txt
-ENTRYPOINT python --version
 EXPOSE $PORT
---
+++ tests/test_schemas.py
@@ -0,0 +1,65 @@
+import pytest
+from app.model.schemas import CustomerBaseSchema
+
+
+@pytest.fixture
+def custom_costumer():
+    return {
+        'corporate_name': 'custom',
+        'address': 'addr',
+        'declared_billing': '1235',
+        'bank_data': [
+            {
+                'bank': 'bank',
+                'account': '123456',
+                'bank_branch': '89874'
+            },
+        ]
+    }
+
+
+def test_valid_costumer(custom_costumer):
+    try:
+        _ = CustomerBaseSchema(**custom_costumer)
+    except ValueError:
+        assert False
+    else:
+        assert True
+
+
+def test_missing_values_costumer():
+    try:
+        test_costumer = {
+            'corporate_name': 'custom',
+            'address': 'addr',
+            'declared_billing': '1235'
+        }
+        _ = CustomerBaseSchema(**test_costumer)
+    except ValueError:
+        assert True
+    else:
+        assert False
+
+
+def test_invalid_account_costumer(custom_costumer):
+    try:
+        test_costumer = custom_costumer
+        test_costumer['bank_data'][0]['account'] = 'abc'
+
+        _ = CustomerBaseSchema(**test_costumer)
+    except ValueError:
+        assert True
+    else:
+        assert False
+
+
+def test_invalid_bank_branch_costumer(custom_costumer):
+    try:
+        test_costumer = custom_costumer
+        test_costumer['bank_data'][0]['bank_branch'] = 'abc'
+
+        _ = CustomerBaseSchema(**test_costumer)
+    except ValueError:
+        assert True
+    else:
+        assert False
